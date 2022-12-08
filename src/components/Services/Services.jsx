842e8543984c7246c57e2dacef7b66d702e1841f
Chetanmilmile <107764112+Chetanmilmile@users.noreply.github.com>
2022-12-08 16:48:00 +0530

    nav add

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,79 +1,10 @@
-import { CardGroup, Col, Container, Row } from 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { Col, Container, Row } from 'react-bootstrap';
+
 import './Services.css'
 
 function Services() {
   return (
 
-
-    // <Container fluid className='Service-container'>
-
-    //         <h1 className='our-services'>OUR SERVICES</h1>
-
-    // <Row >
-    // <Card className='service-card'>
-    //   <img variant="top" src="round.jpg" className='service-img' />
-    //   <Card.Body>
-    //     <Card.Title className='service-title'>ONEWAY DROPS</Card.Title>
-    //     <Card.Text className='service-texts'>
-    //       Our network of over 15 lakh one way routes ensures that there is no corner of the country tht you cant travel with us.Pay only one side charge at rock botoom rates. If you need to be somewhere, we'll get you there.
-    //     </Card.Text>
-    //   </Card.Body>
-    //   <ListGroup className="list-group-flush">
-    //     <ListGroup.Item className='service-text'><img src='hatchback.png' className='service-icons'/><br/>15 Lakh routes </ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='hand.png' className='service-icons'/><br/>Lowest Fares</ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='india.png' className='service-icons'/><br/>All Inclusive Atops</ListGroup.Item>
-    //   </ListGroup>
-    // </Card>
-    // <Card  className='service-card'>
-    //   <Card.Img variant="" src="round.jpg" className='service-img' />
-    //   <img src='road.png' className='service-png'/>
-    //   <Card.Body>
-    //     <Card.Title>ONEWAY DROPS</Card.Title>
-    //     <Card.Text>
-    //       Our network of over 15 lakh one way routes ensures that there is no corner of the country tht you cant travel with us.Pay only one side charge at rock botoom rates. If you need to be somewhere, we'll get you there.
-    //     </Card.Text>
-    //   </Card.Body>
-    //   <ListGroup className="list-group-flush">
-    //     <ListGroup.Item className='service-text'><img src='hatchback.png' className='service-icons'/><br/>15 Lakh routes </ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='hand.png' className='service-icons'/><br/>Lowest Fares</ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='india.png' className='service-icons'/><br/>All Inclusive Atops</ListGroup.Item>
-    //   </ListGroup>
-      
-    // </Card>
-    // <Card  className='service-card'>
-    //   <Card.Img variant="top" src="round.jpg" className='service-img' />
-    //   <Card.Body>
-    //     <Card.Title>ONEWAY DROPS</Card.Title>
-    //     <Card.Text>
-    //       Our network of over 15 lakh one way routes ensures that there is no corner of the country tht you cant travel with us.Pay only one side charge at rock botoom rates. If you need to be somewhere, we'll get you there.
-    //     </Card.Text>
-    //   </Card.Body>
-    //   <ListGroup className="list-group-flush">
-    //     <ListGroup.Item className='service-text'><img src='hatchback.png' className='service-icons'/><br/>15 Lakh routes </ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='hand.png' className='service-icons'/><br/>Lowest Fares</ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='india.png' className='service-icons'/><br/>All Inclusive Atops</ListGroup.Item>
-    //   </ListGroup>
-      
-    // </Card>
-    // <Card  className='service-card'>
-    //   <Card.Img variant="top" src="round.jpg" className='service-img' />
-    //   <Card.Body>
-    //     <Card.Title>ONEWAY DROPS</Card.Title>
-    //     <Card.Text>
-    //       Our network of over 15 lakh one way routes ensures that there is no corner of the country tht you cant travel with us.Pay only one side charge at rock botoom rates. If you need to be somewhere, we'll get you there.
-    //     </Card.Text>
-    //   </Card.Body>
-    //   <ListGroup className="list-group-flush">
-    //     <ListGroup.Item className='service-text'><img src='hatchback.png' className='service-icons'/><br/>15 Lakh routes </ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='hand.png' className='service-icons'/><br/>Lowest Fares</ListGroup.Item>
-    //     <ListGroup.Item className='service-text'><img src='india.png' className='service-icons'/><br/>All Inclusive Atops</ListGroup.Item>
-    //   </ListGroup>
-      
-    // </Card>
-    // </Row>
-    // </Container>
 
     <Container fluid className='services'>
 
